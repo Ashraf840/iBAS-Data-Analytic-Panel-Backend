0c528f826c9c8c936dc9a12bb858c5df91982c97
ZubairAzim <zubair.miazi@doer.com.bd>
2024-01-25 12:59:31 +0600

    import data using command python3 manage.py import_data

--- final_dataset_operations/management/commands/import_data.py
+++ final_dataset_operations/management/commands/import_data.py
@@ -8,69 +8,22 @@ class Command(BaseCommand):
     help = 'Import data from Excel file into FinalDataset model'
 
     def handle(self, *args, **options):
-        # excel_file = '/home/tanjim/workstation/ibas-project/source/ibas_final_dataset.xlsx'
-        excel_file = '/home/robin/workstation/source/Final-updated-dataset.xlsx'
+        excel_file = '/home/zubair/workstation_2/source/Final-updated-dataset.xlsx'
 
         data = pd.read_excel(excel_file, engine='openpyxl')
 
-        # Define a function to detect the language
-        def detect_language(text):
-            try:
-                return detect(text)
-            except:
-                return 'Unknown'
-
-        # Separate questions and answers based on language
-        bangla_questions = []
-        transliterated_questions = []
-        english_questions = []
-        bangla_answers = []
-        english_answers = []
-
         for _, row in data.iterrows():
-            question = row['Questions']
-            answer = row['Answers']
+            bangla_ques = row['bangla_ques']
+            transliterated_ques = row['transliterated_ques']
+            bangla_ans = row['bangla_ans']
+            english_ques = row['english_ques']
+            english_ans = row['english_ans']
 
-            # Detect the language for both question and answer
-            language_question = detect_language(question)
-            language_answer = detect_language(answer)
-
-            if language_question == 'bn' and language_answer == 'bn':
-                bangla_questions.append(question)
-                bangla_answers.append(answer)
-            elif language_question != 'en' and language_answer == 'bn':
-                transliterated_questions.append(question)
-                bangla_answers.append(answer)
-            elif language_question == 'en' and language_answer == 'en':
-                english_questions.append(question)
-                english_answers.append(answer)
-
-        # Print questions and answers by language category
-        self.stdout.write(self.style.SUCCESS("Bangla Questions and Answers:"))
-        for q, a in zip(bangla_questions, bangla_answers):
-            language = "Bangla"
-            entry = FinalDataset(question=q, answer=a, language=language)
+            entry = FinalDataset(
+                bangla_ques=bangla_ques,
+                transliterated_ques=transliterated_ques,
+                bangla_ans=bangla_ans,
+                english_ques=english_ques,
+                english_ans=english_ans,
+            )
             entry.save()
-            self.stdout.write(f"Question: {q}, Answer: {a}, Language: {language}")
-
-        self.stdout.write(self.style.SUCCESS("Transliterated Questions and Bangla Answers:"))
-        for q, a in zip(transliterated_questions, bangla_answers):
-            language = "Transliterated"
-            entry = FinalDataset(question=q, answer=a, language=language)
-            entry.save()
-            self.stdout.write(f"Question: {q}, Answer: {a}, Language: {language}")
-
-        self.stdout.write(self.style.SUCCESS("English Questions and Answers:"))
-        for q, a in zip(english_questions, english_answers):
-            language = "English"
-            entry = FinalDataset(question=q, answer=a, language=language)
-            entry.save()
-            self.stdout.write(f"Question: {q}, Answer: {a}, Language: {language}")
-
-        # Print the FinalDataset table
-        self.stdout.write(self.style.SUCCESS("FinalDataset Table:"))
-        final_dataset_entries = FinalDataset.objects.all()
-        for entry in final_dataset_entries:
-            self.stdout.write(f"Question: {entry.question}, Answer: {entry.answer}, Language: {entry.language}")
-
-        self.stdout.write(self.style.SUCCESS('Data imported successfully.'))
--- src/settings.py
+++ src/settings.py
@@ -92,6 +92,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ibas_chat_bot_operator_qa_db_3',
+#         'USER': 'ibas_chat_bot_operator_qa_admin',
+#         'PASSWORD': '1RNlz7q9y22$',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
