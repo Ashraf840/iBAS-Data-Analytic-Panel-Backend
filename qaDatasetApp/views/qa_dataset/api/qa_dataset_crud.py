23ac83c9ee906766141fa4580b22855e8f90c5e9
ZubairAzim <zubair.miazi@doer.com.bd>
2024-01-28 15:09:57 +0600

    qa-dataset pagination update

--- qaDatasetApp/views/qa_dataset/api/qa_dataset_crud.py
+++ qaDatasetApp/views/qa_dataset/api/qa_dataset_crud.py
@@ -16,6 +16,7 @@ class QADatasetDetail(generics.RetrieveUpdateDestroyAPIView):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
+        print("sth")
         self.perform_update(serializer)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
@@ -30,3 +31,93 @@ class QADatasetList(generics.ListCreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def list(self, request, *args, **kwargs):
+        offset = int(request.query_params.get('offset', 0))
+        limit = int(request.query_params.get('limit', 0))
+        text = request.query_params.get('searchText')
+
+        queryset = self.filter_queryset(self.get_queryset())
+        count = queryset.count()
+        if text:
+            queryset = queryset.filter(
+            bangla_ques__icontains=text,
+            english_ques__icontains=text,
+            transliterated_ques__icontains=text,
+            bangla_ans__icontains=text,
+            english_ans__icontains=text
+            )
+            print ("text")
+
+            count = queryset.count()
+
+        elif limit:
+            queryset = queryset[offset: offset + limit]
+            print("limit")
+        elif limit and text:
+            queryset = queryset.filter(
+                bangla_ques__icontains==text |
+                english_ques__icontains==text |
+                transliterated_ques__icontains==text |
+                bangla_ans__icontains==text |
+                english_ans__icontains==text
+            )[offset:offset + limit]
+            print("limit and text")
+            
+        
+        serializer = self.get_serializer(queryset, many=True)
+
+        data = {
+            'count': count,
+            'results': serializer.data
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
+# class YourModelListView(ListAPIView):
+#     serializer_class = YourModelSerializer
+#     queryset = YourModel.objects.all()
+
+#     def list(self, request, *args, **kwargs):
+#         offset = int(request.query_params.get('offset', 0))
+#         limit = int(request.query_params.get('limit', 0))
+#         text = request.query_params.get('searchText', '')
+
+#         queryset = self.filter_queryset(self.get_queryset())
+
+#         if text:
+#             # Search the text in all relevant fields
+#             queryset = queryset.filter(
+#                 bangla_ques__icontains==text |
+#                 english_ques__icontains==text |
+#                 transliterated_ques__icontains==text |
+#                 bangla_ans__icontains==text |
+#                 english_ans__icontains==text
+#             )
+
+#         count = queryset.count()
+
+#         if limit:
+#             # Apply pagination
+#             queryset = queryset[offset: offset + limit]
+
+#         serializer = self.get_serializer(queryset, many=True)
+
+#         data = {
+#             'count': count,
+#             'results': serializer.data
+#         }
+
+#         return Response(data, status=status.HTTP_200_OK)
+
+# class QADatasetList(generics.ListCreateAPIView):
+#     queryset = qadm.QADataset.objects.filter(flags=False).order_by('id')
+#     serializer_class = qads.QADataset
+#     pagination_class = main.StandardResultsSetPaginationMixin
+#     print("here")
+
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_create(serializer)
+#         print("here")
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
